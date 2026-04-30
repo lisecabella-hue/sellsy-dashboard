16bca6ece271c70c058bb85745f89d63e3f25a9d
lisecabella-hue <lise.cabella@kreme-paris.com>
2026-04-30 15:56:39 +0200

    Update sellsy.js

--- api/sellsy.js
+++ api/sellsy.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     const body = JSON.stringify({
       filters: {
         date: { start: dateStart, end: dateEnd },
-        statuses: ['pending', 'late', 'partial', 'paid', 'cancelled']
+        status: ['payinprogress', 'due', 'paid', 'late', 'cancelled']
       }
     });
 
