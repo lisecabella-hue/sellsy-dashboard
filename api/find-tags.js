e030494c40419e936e81b631dae6cf2c5450ed8b
lisecabella-hue <lise.cabella@kreme-paris.com>
2026-05-04 12:39:54 +0200

    Create find-tags.js

---
+++ api/find-tags.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const clientId = process.env.SELLSY_CLIENT_ID;
+  const clientSecret = process.env.SELLSY_CLIENT_SECRET;
+
+  const tokenResp = await fetch('https://login.sellsy.com/oauth2/access-tokens', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret
+    })
+  });
+  const { access_token } = await tokenResp.json();
+
+  const resp = await fetch('https://api.sellsy.com/v2/smart-tags?limit=50&search=B2C', {
+    headers: { 'Authorization': `Bearer ${access_token}` }
+  });
+  const data = await resp.json();
+  return res.status(200).json(data);
+}
