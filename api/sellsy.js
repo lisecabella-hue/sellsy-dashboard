37d43ecde2296b629efa62e17f8965718ef41c21
lisecabella-hue <lise.cabella@kreme-paris.com>
2026-05-06 12:57:18 +0200

    Update sellsy.js

--- api/sellsy.js
+++ api/sellsy.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     } catch { return null; }
   }
 
+  
   async function cacheSet(key, value, exSeconds) {
     try {
       const encoded = encodeURIComponent(key);
